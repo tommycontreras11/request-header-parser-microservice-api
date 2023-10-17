cbdd5ca8707f32fbc706a359824b44ab1b269584
Tommy Grullón Contreras <tommycontreras34@gmail.com>
2023-10-17 09:22:10 -0400

    Added functionality to get the local pc information

--- src/index.js
+++ src/index.js
@@ -3,4 +3,19 @@ const app = express();
 
 app.listen(3000);
 
-console.log("Hello World");
+app.get('/api/whoami', (req, res) => {
+    fetch('https://api.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+        res.json({
+            ipaddress: data.ip,
+            language: req.headers["accept-language"],
+            software: req.headers["user-agent"]
+        })
+    })
+    .catch(error => {
+      console.error('Error fetching public IP:', error);
+    });
+})
+
+app.set('trust proxy', true);
